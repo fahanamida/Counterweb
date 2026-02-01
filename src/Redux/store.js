d0b775ab7242cf78963d7df3542c8c76ae76c468
fahana <fahanamida@gmail.com>
2026-02-02 01:37:59 +0530

    fix: export mismatch for store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
 import counterSliceReducer from './slices/counterSlice'
 
-const store=configureStore({
+export const store = configureStore({
     reducer:{
         counterReducer:counterSliceReducer,
     }
 })
-
-export default store
